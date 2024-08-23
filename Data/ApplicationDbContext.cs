1f91e7eb93ec2523429ffcf30d9b65343b2def79
Alex <alexandertugin1985@gmail.com>
2024-08-23 17:32:24 +0200

    Initialization

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Models.Category> Category { get; set; }
+    }
+}
