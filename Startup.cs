c2dcdb72a23edf76603725874c85e89e21a7420c
Alex <alexandertugin1985@gmail.com>
2024-09-09 20:54:44 +0200

    Added product page with the ability to add to cart

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,13 @@ namespace OnlineStore
         {
             services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddHttpContextAccessor();
+            services.AddSession(Options =>
+            {
+                Options.IdleTimeout = TimeSpan.FromMinutes(10);
+                Options.Cookie.HttpOnly = true;
+                Options.Cookie.IsEssential = true;
+            });
             services.AddControllersWithViews();
         }
 
@@ -49,6 +56,8 @@ namespace OnlineStore
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSession();
 
             app.UseEndpoints(endpoints =>
             {
