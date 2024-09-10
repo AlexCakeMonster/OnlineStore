d49766379997124cb5b2ef7df454cdffd989a891
Alex <alexandertugin1985@gmail.com>
2024-09-10 11:41:19 +0200

    Added controllers for adding a product to the cart and deleting the cart.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using OnlineStore.Data;
 using OnlineStore.Models;
 using OnlineStore.Models.ViewModels;
+using OnlineStore.Utility;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -35,6 +36,13 @@ namespace OnlineStore.Controllers
 
         public IActionResult Details(int Id)
         {
+            List<ShoppingCart> shoppingCartsList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                shoppingCartsList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
             DetailsVm detailsVm = new DetailsVm()
             {
                 Product = _db.Product.Include(u => u.Category).Include(u => u.ApplicationType)
@@ -42,9 +50,47 @@ namespace OnlineStore.Controllers
                 ExistsInCart = false
             };
 
+            foreach(var el in shoppingCartsList)
+            {
+                if(el.ProductId == Id)
+                {
+                    detailsVm.ExistsInCart = true;
+                }
+            }
             return View(detailsVm);
         }
+        [HttpPost, ActionName("Details")]
+        public IActionResult DetailsPost(int Id)
+        {
+            List<ShoppingCart> shoppingCartsList = new List<ShoppingCart>();
+            if(HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart)!= null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                shoppingCartsList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+            shoppingCartsList.Add(new ShoppingCart { ProductId = Id });
+            HttpContext.Session.Set(WC.SessionCart, shoppingCartsList);
+            return RedirectToAction(nameof(Index));
+        }
+       
+        public IActionResult RemoveFromCart(int Id)
+        {
+            List<ShoppingCart> shoppingCartsList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                shoppingCartsList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
 
+            var itemToRemove = shoppingCartsList.SingleOrDefault(r => r.ProductId == Id);
+            if(itemToRemove != null)
+            {
+                shoppingCartsList.Remove(itemToRemove);
+            }
+            
+            HttpContext.Session.Set(WC.SessionCart, shoppingCartsList);
+            return RedirectToAction(nameof(Index));
+        }
         public IActionResult Privacy()
         {
             return View();
@@ -55,5 +101,7 @@ namespace OnlineStore.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
     }
 }
--- WC.cs
+++ WC.cs
@@ -8,5 +8,6 @@ namespace OnlineStore
     public static class WC
     {
         public static string ImagePath = @"\images\product\";
+        public static string SessionCart = "ShoppingCartSession";
     }
 }
